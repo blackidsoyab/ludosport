493f9cfb1325890332092b45aece8a4551ec8029
Rana Soyab <soyab@blackidsolutions.com>
2014-07-26 18:17:57 +0530

    new db

--- ludosport.sql
+++ ludosport.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 25, 2014 at 05:29 PM
+-- Generation Time: Jul 26, 2014 at 06:17 PM
 -- Server version: 5.5.38-0ubuntu0.12.04.1
 -- PHP Version: 5.3.10-1ubuntu3.13
 
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `academies` (
 
 INSERT INTO `academies` (`id`, `rector_id`, `en_academy_name`, `it_academy_name`, `type`, `contact_firstname`, `contact_lastname`, `association_fullname`, `role_referent`, `address`, `postal_code`, `city_id`, `state_id`, `country_id`, `phone_1`, `phone_2`, `email`, `user_id`, `timestamp`) VALUES
 (1, '3,5', 'Poppey Sailor Man', 'Poppey Sailor Man', 'ac', 'Soyab', 'Rana', 'PSM', 'Poppeyyyyyyyyyy Sailorrrrrrrrrrr Mannnnnnnnnnnnnnnnn', 'Baroda', '390016', 1, 1, 1, '91987654321', '91987654321', 'demo@yopmail.com', 2, '2014-07-25 08:45:52'),
-(2, '3', 'Dexter Laboratory', 'Dexter Laboratory', 'ac', 'Soyab', 'Rana', 'PSM', 'Temparory', 'Baroda', '390016', 1, 1, 1, '91987654321', '91987654321', 'demo@yopmail.com', 2, '2014-07-25 11:22:54');
+(2, '6,3', 'Dexter Laboratory', 'Dexter Laboratory', 'ac', 'Soyab', 'Rana', 'PSM', 'Temparory', 'Baroda', '390016', 1, 1, 1, '91987654321', '91987654321', 'demo@yopmail.com', 2, '2014-07-25 11:22:54');
 
 -- --------------------------------------------------------
 
@@ -109,7 +109,17 @@ CREATE TABLE IF NOT EXISTS `clans` (
   `email` varchar(65) NOT NULL,
   `user_id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `clans`
+--
+
+INSERT INTO `clans` (`id`, `academy_id`, `school_id`, `teacher_id`, `level_id`, `lesson_day`, `lesson_from`, `lesson_to`, `en_class_name`, `it_class_name`, `same_address`, `address`, `postal_code`, `city_id`, `state_id`, `country_id`, `phone_1`, `phone_2`, `email`, `user_id`, `timestamp`) VALUES
+(1, 1, 1, '5', 1, '1,2', 1406345400, 1406356200, 'Poppey Ep 1', 'Poppey Ep 1', 1, 'Baroda', '390016', 1, 1, 1, '91987654321', '91987654321', 'demo@yopmail.com', 2, '2014-07-26 09:45:00'),
+(2, 2, 2, '7', 2, '2,3', 1406349000, 1406356200, 'Dexter Ep 1', 'Dexter Ep 1', 1, 'Baroda', '390016', 1, 1, 1, '91987654321', '91987654321', 'demo@yopmail.com', 2, '2014-07-26 09:46:59'),
+(3, 1, 3, '3', 1, '1,3,4', 1406352600, 1406359800, 'Sailor Ep 2', 'Sailor Ep 2', 1, 'Baroda', '390016', 1, 1, 1, '91987654321', '91987654321', 'soyab@yopmail.com', 2, '2014-07-26 09:47:38'),
+(4, 2, 4, '8', 1, '1', 1406363400, 1406370600, 'Lab Ep 2', 'Lab Ep 2', 1, 'Baroda', '390016', 1, 1, 1, '91987654321', '91987654321', 'soyab@yopmail.com', 2, '2014-07-26 09:48:27');
 
 -- --------------------------------------------------------
 
@@ -138,6 +148,32 @@ INSERT INTO `countries` (`id`, `en_name`, `it_name`, `user_id`, `timestamp`) VAL
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `emails`
+--
+
+CREATE TABLE IF NOT EXISTS `emails` (
+`id` int(11) NOT NULL,
+  `type` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `subject` text CHARACTER SET utf8,
+  `message` text CHARACTER SET utf8,
+  `attachment` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  `format_info` text CHARACTER SET utf8,
+  `user_id` int(11) NOT NULL DEFAULT '1',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `emails`
+--
+
+INSERT INTO `emails` (`id`, `type`, `subject`, `message`, `attachment`, `format_info`, `user_id`, `timestamp`) VALUES
+(1, 'user_registration', 'User Registration', 'Hello #firstname #lastname<div><br></div><div>Thanks for Registration.</div><div><br></div><div>Thanks,</div><div>MyLudosport Team</div>', NULL, '#firstname\r\n#lastname', 1, '2014-07-26 07:15:22'),
+(2, 'forgot_password', 'Forgot Password', 'Hello #firstname #lastname <div><br></div><div>You have request for the reset the password.</div><div>Please click the below link to reset password.<br>\n#reset_link</div><div><br></div><div>Thanks,</div><div>MyLudosport Team</div>', NULL, '#firstname\r\n#lastname\r\n#reset_link', 1, '2014-07-26 07:15:22'),
+(3, 'user_registration_notification', 'New User Registration Notification', 'New User<div><br></div><div>#firstname #lastname is registerd on #date<div><br></div><div><div>Thanks,</div><div>MyLudosport Team</div></div></div>', NULL, '#firstname\r\n#lastname\r\n#date', 1, '2014-07-26 07:15:22');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `levels`
 --
 
@@ -160,28 +196,82 @@ INSERT INTO `levels` (`id`, `en_level_name`, `it_level_name`, `is_basic`, `under
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `mailboxes`
+--
+
+CREATE TABLE IF NOT EXISTS `mailboxes` (
+`id` int(11) NOT NULL,
+  `type` enum('L','N','H','U') NOT NULL DEFAULT 'N',
+  `to_email` varchar(65) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `attachment` varchar(255) DEFAULT NULL,
+  `status` int(1) NOT NULL DEFAULT '0',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `mailboxes`
+--
+
+INSERT INTO `mailboxes` (`id`, `type`, `to_email`, `subject`, `message`, `attachment`, `status`, `timestamp`) VALUES
+(1, 'L', 'davidbid@youmail.com', 'User Registration', 'Hello David Leo<div><br></div><div>Thanks for Registration.</div><div><br></div><div>Thanks,</div><div>MyLudosport Team</div>', NULL, 0, '2014-07-26 11:10:31'),
+(2, 'L', 'soyab@yopmail.com', 'New User Registration Notification', 'New User<div><br></div><div>David Leo is registerd on 2014-07-26 16:40:31<div><br></div><div><div>Thanks,</div><div>MyLudosport Team</div></div></div>', NULL, 0, '2014-07-26 11:10:31'),
+(3, 'L', 'soyab@yopmail.com', 'New User Registration Notification', 'New User<div><br></div><div>David Leo is registerd on 2014-07-26 16:40:31<div><br></div><div><div>Thanks,</div><div>MyLudosport Team</div></div></div>', NULL, 0, '2014-07-26 11:10:31'),
+(4, 'L', 'soyab@yopmail.com', 'New User Registration Notification', 'New User<div><br></div><div>David Leo is registerd on 2014-07-26 16:40:31<div><br></div><div><div>Thanks,</div><div>MyLudosport Team</div></div></div>', NULL, 0, '2014-07-26 11:10:31'),
+(5, 'L', 'soyab@yopmail.com', 'New User Registration Notification', 'New User<div><br></div><div>David Leo is registerd on 2014-07-26 16:40:31<div><br></div><div><div>Thanks,</div><div>MyLudosport Team</div></div></div>', NULL, 0, '2014-07-26 11:10:31'),
+(6, 'L', 'soyab@yopmail.com', 'New User Registration Notification', 'New User<div><br></div><div>David Leo is registerd on 2014-07-26 16:40:31<div><br></div><div><div>Thanks,</div><div>MyLudosport Team</div></div></div>', NULL, 0, '2014-07-26 11:10:31'),
+(7, 'L', 'soyab@yopmail.com', 'New User Registration Notification', 'New User<div><br></div><div>David Leo is registerd on 2014-07-26 16:40:31<div><br></div><div><div>Thanks,</div><div>MyLudosport Team</div></div></div>', NULL, 0, '2014-07-26 11:10:31'),
+(8, 'L', 'soyab@yopmail.com', 'New User Registration Notification', 'New User<div><br></div><div>David Leo is registerd on 2014-07-26 16:40:31<div><br></div><div><div>Thanks,</div><div>MyLudosport Team</div></div></div>', NULL, 0, '2014-07-26 11:10:32');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `notifications`
 --
 
 CREATE TABLE IF NOT EXISTS `notifications` (
 `id` int(11) NOT NULL,
-  `notify_type` enum('rector_assign_academy','dean_assign_school','teacher_assign_class') NOT NULL,
+  `type` enum('A','I','W','N') NOT NULL DEFAULT 'N',
+  `notify_type` enum('rector_assign_academy','dean_assign_school','teacher_assign_class','user_register') NOT NULL,
   `from_id` int(11) NOT NULL,
   `to_id` int(11) NOT NULL,
   `object_id` int(11) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '0',
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
 
 --
 -- Dumping data for table `notifications`
 --
 
-INSERT INTO `notifications` (`id`, `notify_type`, `from_id`, `to_id`, `object_id`, `status`, `timestamp`) VALUES
-(1, 'rector_assign_academy', 2, 3, 1, 0, '2014-07-25 08:45:52'),
-(2, 'rector_assign_academy', 2, 5, 1, 0, '2014-07-25 08:45:52'),
-(5, 'dean_assign_school', 2, 3, 0, 0, '2014-07-25 10:49:30'),
-(6, 'rector_assign_academy', 2, 3, 2, 0, '2014-07-25 11:22:55');
+INSERT INTO `notifications` (`id`, `type`, `notify_type`, `from_id`, `to_id`, `object_id`, `status`, `timestamp`) VALUES
+(1, 'N', 'rector_assign_academy', 2, 3, 1, 1, '2014-07-25 08:45:52'),
+(2, 'N', 'rector_assign_academy', 2, 5, 1, 0, '2014-07-25 08:45:52'),
+(5, 'N', 'dean_assign_school', 2, 3, 1, 1, '2014-07-25 10:49:30'),
+(6, 'N', 'rector_assign_academy', 2, 3, 2, 1, '2014-07-25 11:22:55'),
+(7, 'N', 'dean_assign_school', 2, 3, 1, 1, '2014-07-26 05:43:28'),
+(8, 'N', 'teacher_assign_class', 2, 3, 1, 1, '2014-07-26 05:45:01'),
+(9, 'N', 'rector_assign_academy', 2, 3, 1, 1, '2014-07-26 05:45:22'),
+(10, 'N', 'dean_assign_school', 2, 3, 2, 1, '2014-07-26 05:49:39'),
+(12, 'N', 'teacher_assign_class', 2, 3, 1, 1, '2014-07-26 06:36:03'),
+(13, 'N', 'dean_assign_school', 2, 4, 4, 0, '2014-07-26 09:43:41'),
+(14, 'N', 'dean_assign_school', 2, 4, 3, 0, '2014-07-26 09:44:24'),
+(15, 'N', 'teacher_assign_class', 2, 5, 1, 0, '2014-07-26 09:45:00'),
+(16, 'N', 'teacher_assign_class', 2, 7, 2, 0, '2014-07-26 09:47:00'),
+(17, 'N', 'teacher_assign_class', 2, 7, 3, 0, '2014-07-26 09:47:38'),
+(18, 'N', 'teacher_assign_class', 2, 8, 4, 0, '2014-07-26 09:48:27'),
+(19, 'I', 'user_register', 0, 2, 9, 0, '2014-07-26 11:10:31'),
+(20, 'I', 'user_register', 0, 3, 9, 1, '2014-07-26 11:10:31'),
+(21, 'I', 'user_register', 0, 4, 9, 0, '2014-07-26 11:10:31'),
+(22, 'I', 'user_register', 0, 5, 9, 0, '2014-07-26 11:10:31'),
+(23, 'I', 'user_register', 0, 6, 9, 0, '2014-07-26 11:10:31'),
+(24, 'I', 'user_register', 0, 7, 9, 0, '2014-07-26 11:10:31'),
+(25, 'I', 'user_register', 0, 8, 9, 0, '2014-07-26 11:10:31'),
+(27, 'N', 'rector_assign_academy', 0, 0, 0, 1, '2014-07-26 12:31:56'),
+(28, 'N', 'rector_assign_academy', 0, 0, 0, 1, '2014-07-26 12:36:03'),
+(29, 'N', 'rector_assign_academy', 0, 0, 0, 1, '2014-07-26 12:42:47'),
+(30, 'N', 'rector_assign_academy', 0, 0, 0, 1, '2014-07-26 12:43:04');
 
 -- --------------------------------------------------------
 
@@ -255,7 +345,7 @@ INSERT INTO `roles` (`id`, `en_role_name`, `it_role_name`, `permission`, `is_del
 --
 
 CREATE TABLE IF NOT EXISTS `schools` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `academy_id` int(11) NOT NULL,
   `dean_id` varchar(11) NOT NULL,
   `en_school_name` varchar(65) NOT NULL,
@@ -270,15 +360,17 @@ CREATE TABLE IF NOT EXISTS `schools` (
   `email` varchar(65) NOT NULL,
   `user_id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `schools`
 --
 
 INSERT INTO `schools` (`id`, `academy_id`, `dean_id`, `en_school_name`, `it_school_name`, `address`, `postal_code`, `city_id`, `state_id`, `country_id`, `phone_1`, `phone_2`, `email`, `user_id`, `timestamp`) VALUES
-(0, 1, '3', 'Poppey', 'Poppey', 'Baroda', 390016, 1, 1, 1, '91987654321', '91987654321', 'demo@yopmail.com', 2, '2014-07-25 10:49:30'),
-(0, 2, '3', 'Dexter', 'Dexter', 'Baroda', 390016, 1, 1, 1, '91987654321', '91987654321', 'demo@yopmail.com', 2, '2014-07-25 11:23:56');
+(1, 1, '3', 'Poppey', 'Poppey', 'Baroda', 390016, 1, 1, 1, '91987654321', '91987654321', 'demo@yopmail.com', 2, '2014-07-25 10:49:30'),
+(2, 2, '3', 'Dexter', 'Dexter', 'Baroda', 390016, 1, 1, 1, '91987654321', '91987654321', 'demo@yopmail.com', 2, '2014-07-25 11:23:56'),
+(3, 1, '4', 'Salior', 'Salior', 'Baroda', 390016, 1, 1, 1, '91987654321', '91987654321', 'soyab@yopmail.com', 2, '2014-07-26 09:43:41'),
+(4, 2, '4', 'Laboratory', 'Laboratory', 'Baroda', 390016, 1, 1, 1, '91987654321', '91987654321', 'soyab@yopmail.com', 2, '2014-07-26 09:44:24');
 
 -- --------------------------------------------------------
 
@@ -335,6 +427,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `date_of_birth` bigint(100) NOT NULL,
   `city_id` int(11) NOT NULL,
   `permission` longtext,
+  `avtar` varchar(255) NOT NULL DEFAULT 'no_avatar.jpg',
   `status` enum('A','D','P') NOT NULL DEFAULT 'P',
   `user_id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -344,16 +437,16 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `role_id`, `username`, `password`, `firstname`, `lastname`, `email`, `date_of_birth`, `city_id`, `permission`, `status`, `user_id`, `timestamp`) VALUES
-(1, '1', 'superadmin', '202cb962ac59075b964b07152d234b70', 'Soyab', 'Rana', 'soyab@blackidsolutions.com', 1990, 0, NULL, 'A', 0, '2014-07-17 07:05:53'),
-(2, '2', 'admin', '202cb962ac59075b964b07152d234b70', 'Admin', 'James', 'admin@admin.com', 1990, 1, NULL, 'A', 0, '2014-07-17 07:28:01'),
-(3, '3,4,5', 'rector_1', '202cb962ac59075b964b07152d234b70', 'Rector', '1', 'rector1@rector.com', -19800, 1, NULL, 'A', 2, '2014-07-17 07:28:01'),
-(4, '4', 'dean_1', '202cb962ac59075b964b07152d234b70', 'Dean', '1', 'dean@dean.com', 1990, 1, NULL, 'A', 0, '2014-07-17 07:28:01'),
-(5, '5,3', 'teacher_1', '202cb962ac59075b964b07152d234b70', 'Teacher', '1', 'teacher1@teacher.com', 653682600, 2, NULL, 'A', 1, '2014-07-21 10:11:41'),
-(6, '3', 'rector_2', '202cb962ac59075b964b07152d234b70', 'Rector', '2', 'rector2@rector.com', -19800, 1, NULL, 'A', 2, '2014-07-17 07:28:01'),
-(7, '5', 'teacher_2', '202cb962ac59075b964b07152d234b70', 'Teacher', '2', 'teacher2@teacher.com', 653682600, 2, NULL, 'A', 1, '2014-07-21 10:11:41'),
-(8, '5', 'teacher_3', '202cb962ac59075b964b07152d234b70', 'Teacher', '3', 'teacher3@teacher.com', 653682600, 2, NULL, 'A', 1, '2014-07-21 10:11:41'),
-(9, '6', 'u1', '202cb962ac59075b964b07152d234b70', 'User', '1', 'user@user.com', 653682600, 2, NULL, 'P', 1, '2014-07-21 10:11:41');
+INSERT INTO `users` (`id`, `role_id`, `username`, `password`, `firstname`, `lastname`, `email`, `date_of_birth`, `city_id`, `permission`, `avtar`, `status`, `user_id`, `timestamp`) VALUES
+(1, '1', 'superadmin', '202cb962ac59075b964b07152d234b70', 'Soyab', 'Rana', 'soyab@yopmail.com', 1990, 0, NULL, 'soyab.jpg', 'A', 0, '2014-07-17 07:05:53'),
+(2, '2', 'admin', '202cb962ac59075b964b07152d234b70', 'Admin', 'James', 'soyab@yopmail.com', 1990, 1, NULL, 'soyab.jpg', 'A', 0, '2014-07-17 07:28:01'),
+(3, '3,4,5', 'rector_1', '202cb962ac59075b964b07152d234b70', 'Rector', '1', 'soyab@yopmail.com', -19800, 1, NULL, 'no_avatar.jpg', 'A', 2, '2014-07-17 07:28:01'),
+(4, '4', 'dean_1', '202cb962ac59075b964b07152d234b70', 'Dean', '1', 'soyab@yopmail.com', 1990, 1, NULL, 'no_avatar.jpg', 'A', 0, '2014-07-17 07:28:01'),
+(5, '5,3', 'teacher_1', '202cb962ac59075b964b07152d234b70', 'Teacher', '1', 'soyab@yopmail.com', 653682600, 2, NULL, 'no_avatar.jpg', 'A', 1, '2014-07-21 10:11:41'),
+(6, '3', 'rector_2', '202cb962ac59075b964b07152d234b70', 'Rector', '2', 'soyab@yopmail.com', -19800, 1, NULL, 'no_avatar.jpg', 'A', 2, '2014-07-17 07:28:01'),
+(7, '5', 'teacher_2', '202cb962ac59075b964b07152d234b70', 'Teacher', '2', 'soyab@yopmail.com', 653682600, 2, NULL, 'no_avatar.jpg', 'A', 1, '2014-07-21 10:11:41'),
+(8, '5', 'teacher_3', '202cb962ac59075b964b07152d234b70', 'Teacher', '3', 'soyab@yopmail.com', 653682600, 2, NULL, 'no_avatar.jpg', 'A', 1, '2014-07-21 10:11:41'),
+(9, '6', 'user1', '202cb962ac59075b964b07152d234b70', 'David', 'Leo', 'soyab@yopmail.com', 0, 1, NULL, 'no_avatar.jpg', 'P', 0, '2014-07-26 11:10:31');
 
 --
 -- Indexes for dumped tables
@@ -384,9 +477,21 @@ ALTER TABLE `countries`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `emails`
+--
+ALTER TABLE `emails`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `levels`
 --
 ALTER TABLE `levels`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mailboxes`
+--
+ALTER TABLE `mailboxes`
  ADD PRIMARY KEY (`id`);
 
 --
@@ -405,6 +510,12 @@ ALTER TABLE `permissions`
 -- Indexes for table `roles`
 --
 ALTER TABLE `roles`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `schools`
+--
+ALTER TABLE `schools`
  ADD PRIMARY KEY (`id`);
 
 --
@@ -443,22 +554,32 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 -- AUTO_INCREMENT for table `clans`
 --
 ALTER TABLE `clans`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `countries`
 --
 ALTER TABLE `countries`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
+-- AUTO_INCREMENT for table `emails`
+--
+ALTER TABLE `emails`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT for table `levels`
 --
 ALTER TABLE `levels`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
+-- AUTO_INCREMENT for table `mailboxes`
+--
+ALTER TABLE `mailboxes`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT for table `permissions`
 --
@@ -469,6 +590,11 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 ALTER TABLE `roles`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `schools`
+--
+ALTER TABLE `schools`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `states`
 --
