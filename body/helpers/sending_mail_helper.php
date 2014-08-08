658514116ddc3fd03bba60e9af933fd0d2146d12
Rana Soyab <soyab@blackidsolutions.com>
2014-08-08 10:40:04 +0530

    Added Setting for mail

--- body/helpers/sending_mail_helper.php
+++ body/helpers/sending_mail_helper.php
@@ -5,17 +5,29 @@ if (!function_exists('send_mail')) {
     function send_mail($options) {
 
         $ci = get_instance();
-        $config = Array(
-            'protocol' => 'smtp',
-            'smtp_host' => 'ssl://smtp.gmail.com',
-            'smtp_port' => 465,
-            'smtp_user' => 'soyab@blackidsolutions.com',
-            'smtp_pass' => 'soyabsoyab',
-            'mailtype' => 'html',
-        );
+
+        $config['protocol'] = $ci->config->item('protocol');
+        if ($ci->config->item('smtp_port') != 0) {
+            $config['smtp_port'] = $ci->config->item('smtp_port');
+        }
+        $config['smtp_host'] = $ci->config->item('smtp_host');
+        $config['smtp_user'] = $ci->config->item('smtp_user');
+        $config['smtp_pass'] = $ci->config->item('smtp_pass');
+        $config['mailtype'] = 'html';
+
+
+        /*  $config = Array(
+          'protocol' => 'smtp',
+          'smtp_host' => 'ssl://smtp.gmail.com',
+          'smtp_port' => 465,
+          'smtp_user' => 'soyab@blackidsolutions.com',
+          'smtp_pass' => 'soyabsoyab',
+          'mailtype' => 'html',
+          ); */
+
         $ci->load->library('email', $config);
         $ci->email->set_newline("\r\n");
-        $ci->email->from('soyab@blackidsolutions.com', 'MyLudosport');
+        $ci->email->from($ci->config->item('smtp_user'), 'MyLudosport');
         $ci->email->to($options['tomailid']);
         $ci->email->subject($options['subject']);
         $ci->email->message($options['message']);
@@ -27,7 +39,8 @@ if (!function_exists('send_mail')) {
             $ci->email->attach($options['attachement']);
 
         if (!$ci->email->send()) {
-            return FALSE;
+            //return FALSE;
+            return $ci->email->print_debugger();
         } else {
             return TRUE;
         }
