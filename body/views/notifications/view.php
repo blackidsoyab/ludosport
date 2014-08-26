99175a7b4073e02d6a26d482c75eba17514ad62a
Rana Soyab <soyab@blackidsolutions.com>
2014-08-26 17:10:36 +0530

    dynaimc content loading notification

--- body/views/notifications/view.php
+++ body/views/notifications/view.php
@@ -1,44 +1,33 @@
 <script>
-    $(document).ready(function() {
-        var track_load = 0; //total loaded record group(s)
-        var loading  = false; //to prevents multipal ajax loads
-        var total_groups = <?php echo $per_page; ?>; //total record group(s)
-    
-        $('#results').load("<?php echo base_url() . 'load_more_notification/'; ?>" + track_load , function() {track_load++;}); //load first group
-    
-        $(window).scroll(function() { //detect page scroll
-        
-            if($(window).scrollTop() + $(window).height() == $(document).height())  //user scrolled to bottom of the page?
-            {
-            
-                if(track_load <= total_groups && loading==false) //there's more data to load
-                {
-                    loading = true; //prevent further ajax loading
-                    $('.animation_image').show(); //show loading image
-                
-                    //load data from the server using a HTTP POST request
-                    $.post('<?php echo base_url() . 'load_more_notification/'; ?>' + track_load , function(data){
-                                    
-                        $("#results").append(data); //append received data into the element
+$(document).ready(function() {
+    var track_load = 0;
+    var loading  = false;
+    var total_groups = <?php echo $per_page; ?>;
 
-                        //hide loading image
-                        $('.animation_image').hide(); //hide loading image once data is received
-                    
-                        track_load++; //loaded group increment
-                        loading = false; 
-                
-                    }).fail(function(xhr, ajaxOptions, thrownError) { //any errors?
-                    
-                        alert(thrownError); //alert with HTTP error
-                        $('.animation_image').hide(); //hide loading image
-                        loading = false;
-                
-                    });
-                
-                }
+    $('#results').load("<?php echo base_url() . 'load_more_notification/'; ?>" + track_load , function() {track_load++;});
+
+    $(window).scroll(function() {
+        if($(window).scrollTop() + $(window).height() == $(document).height())
+        {
+            if(track_load <= total_groups && loading==false)
+            {
+                PositionFooter();
+                loading = true;
+                $('.animation_image').show();
+                $.post('<?php echo base_url() . 'load_more_notification/'; ?>' + track_load , function(data){
+                    $("#results").append(data);
+                    $('.animation_image').hide();
+                    track_load++;
+                    loading = false; 
+                }).fail(function(xhr, ajaxOptions, thrownError) {
+                    $('.animation_image').hide();
+                    loading = false;
+                });
+            
             }
-        });
+        }
     });
+});
 </script>
 
 <div class="row">
