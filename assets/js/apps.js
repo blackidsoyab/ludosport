8cb89d77e9da450d7057fe6632269be03cc59d8e
Soyab Rana <soyab@blackidsolutions.com>
2014-09-05 15:16:08 +0530

    onselect file get file name in text box

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -103,6 +103,30 @@ $(document).ready(function(){
 		});
 	});
 	/** END BACK TO TOP **/
+
+	/** BEGIN INPUT FILE **/
+	if ($('.btn-file').length > 0){
+		$(document)
+			.on('change', '.btn-file :file', function() {
+				"use strict";
+				var input = $(this),
+				numFiles = input.get(0).files ? input.get(0).files.length : 1,
+				label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+				input.trigger('fileselect', [numFiles, label]);
+		});
+		$('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+			
+			var input = $(this).parents('.input-group').find(':text'),
+				log = numFiles > 1 ? numFiles + ' files selected' : label;
+			
+			if( input.length ) {
+				input.val(log);
+			} else {
+				if( log ) alert(log);
+			}
+		});
+	}
+	/** END INPUT FILE **/
 	
 	
 	/** NICESCROLL AND SLIMSCROLL FUNCTION **/
