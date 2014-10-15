de4d325bf8bf83ec6ad38fe6cf3b403881ed1386
Soyab Rana <soyab@blackidsolutions.com>
2014-10-15 10:59:23 +0530

    Pupil Design Testing

--- body/views/students/top_ten_rating.php
+++ body/views/students/top_ten_rating.php
@@ -16,7 +16,7 @@
 
 <div class="the-box">
 	<div class="row">
-		<div class="col-sm-6">
+		<div class="col-sm-6 col-md-6 col-lg-6">
 			<div class="panel with-nav-tabs panel-warning">
 				<div class="panel-heading">
 					<ul class="nav nav-tabs">
@@ -31,101 +31,107 @@
 						<div class="tab-content">
 							<div class="tab-pane fade in active" id="tab-xp">
 								<?php if($top_ten_xpr != false) { ?>
-									<table class="table table-th-block margin-bottom-killer">
-										<thead>
-											<tr>
-												<th width="5%"><?php echo $this->lang->line('no'); ?></th>
-												<th width="55%"><?php echo $this->lang->line('name'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('score'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('academy'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('school'); ?></th>
-											</tr>
-										</thead>
-										<tbody>
-											<?php 
-												$count = 0;
-												foreach ($top_ten_xpr as $xpr) {
-											?>
-											<tr>
-												<td><?php echo ++$count; ?></td>
-												<td><?php echo ucwords($xpr->name); ?></td>
-												<td><span  data-toggle="tooltip" data-original-title="<?php echo $this->lang->line('xpr'),':',$xpr->xpr,',', $this->lang->line('war'),':',$xpr->war,',', $this->lang->line('sty'),':',$xpr->sty; ?>"><?php echo $xpr->total_score; ?></span></td>
-												<td><?php echo @$xpr->academy; ?></td>
-												<td><?php echo @$xpr->school; ?></td>
-											</tr>
-											<?php } ?>
-										</tbody>
-									</table>
+									<div class="table-responsive">
+										<table class="table table-th-block margin-bottom-killer">
+											<thead>
+												<tr>
+													<th width="5%"><?php echo $this->lang->line('no'); ?></th>
+													<th width="55%"><?php echo $this->lang->line('name'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('score'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('academy'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('school'); ?></th>
+												</tr>
+											</thead>
+											<tbody>
+												<?php 
+													$count = 0;
+													foreach ($top_ten_xpr as $xpr) {
+												?>
+												<tr>
+													<td><?php echo ++$count; ?></td>
+													<td><?php echo ucwords($xpr->name); ?></td>
+													<td><span  data-toggle="tooltip" data-original-title="<?php echo $this->lang->line('xpr'),':',$xpr->xpr,',', $this->lang->line('war'),':',$xpr->war,',', $this->lang->line('sty'),':',$xpr->sty; ?>"><?php echo $xpr->total_score; ?></span></td>
+													<td><?php echo @$xpr->academy; ?></td>
+													<td><?php echo @$xpr->school; ?></td>
+												</tr>
+												<?php } ?>
+											</tbody>
+										</table>
+									</div>
 								<?php } ?>
 							</div>
 
 							<div class="tab-pane fade" id="tab-war">
 								<?php if($top_ten_war != false) { ?>
-									<table class="table table-th-block margin-bottom-killer">
-										<thead>
-											<tr>
-												<th width="5%"><?php echo $this->lang->line('no'); ?></th>
-												<th width="55%"><?php echo $this->lang->line('name'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('score'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('academy'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('school'); ?></th>
-											</tr>
-										</thead>
-										<tbody>
-											<?php 
-												$count = 0;
-												foreach ($top_ten_war as $war) {
-											?>
-											<tr>
-												<td><?php echo ++$count; ?></td>
-												<td><?php echo ucwords($war->name); ?></td>
-												<td><span  data-toggle="tooltip" data-original-title="<?php echo $this->lang->line('xpr'),':',$war->xpr,',', $this->lang->line('war'),':',$war->war,',', $this->lang->line('sty'),':',$war->sty; ?>"><?php echo $war->total_score; ?></span></td>
-												<td><?php echo @$war->academy; ?></td>
-												<td><?php echo @$war->school; ?></td>
-											</tr>
-											<?php } ?>
-										</tbody>
-									</table>
+									<div class="table-responsive">
+										<table class="table table-th-block margin-bottom-killer">
+											<thead>
+												<tr>
+													<th width="5%"><?php echo $this->lang->line('no'); ?></th>
+													<th width="55%"><?php echo $this->lang->line('name'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('score'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('academy'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('school'); ?></th>
+												</tr>
+											</thead>
+											<tbody>
+												<?php 
+													$count = 0;
+													foreach ($top_ten_war as $war) {
+												?>
+												<tr>
+													<td><?php echo ++$count; ?></td>
+													<td><?php echo ucwords($war->name); ?></td>
+													<td><span  data-toggle="tooltip" data-original-title="<?php echo $this->lang->line('xpr'),':',$war->xpr,',', $this->lang->line('war'),':',$war->war,',', $this->lang->line('sty'),':',$war->sty; ?>"><?php echo $war->total_score; ?></span></td>
+													<td><?php echo @$war->academy; ?></td>
+													<td><?php echo @$war->school; ?></td>
+												</tr>
+												<?php } ?>
+											</tbody>
+										</table>
+									</div>
 								<?php } ?>
 							</div>
 
 							<div class="tab-pane fade" id="tab-style">
 								<?php if($top_ten_sty != false) { ?>
-									<table class="table table-th-block margin-bottom-killer">
-										<thead>
-											<tr>
-												<th width="5%"><?php echo $this->lang->line('no'); ?></th>
-												<th width="55%"><?php echo $this->lang->line('name'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('score'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('academy'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('school'); ?></th>
-											</tr>
-										</thead>
-										<tbody>
-											<?php 
-												$count = 0;
-												foreach ($top_ten_sty as $sty) {
-											?>
-											<tr>
-												<td><?php echo ++$count; ?></td>
-												<td><?php echo ucwords($sty->name); ?></td>
-												<td><span  data-toggle="tooltip" data-original-title="<?php echo $this->lang->line('xpr'),':',$sty->xpr,',', $this->lang->line('war'),':',$sty->war,',', $this->lang->line('sty'),':',$sty->sty; ?>"><?php echo $sty->total_score; ?></span></td>
-												<td><?php echo @$sty->academy; ?></td>
-												<td><?php echo @$sty->school; ?></td>
-											</tr>
-											<?php } ?>
-										</tbody>
-									</table>
+									<div class="table-responsive">
+										<table class="table table-th-block margin-bottom-killer">
+											<thead>
+												<tr>
+													<th width="5%"><?php echo $this->lang->line('no'); ?></th>
+													<th width="55%"><?php echo $this->lang->line('name'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('score'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('academy'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('school'); ?></th>
+												</tr>
+											</thead>
+											<tbody>
+												<?php 
+													$count = 0;
+													foreach ($top_ten_sty as $sty) {
+												?>
+												<tr>
+													<td><?php echo ++$count; ?></td>
+													<td><?php echo ucwords($sty->name); ?></td>
+													<td><span  data-toggle="tooltip" data-original-title="<?php echo $this->lang->line('xpr'),':',$sty->xpr,',', $this->lang->line('war'),':',$sty->war,',', $this->lang->line('sty'),':',$sty->sty; ?>"><?php echo $sty->total_score; ?></span></td>
+													<td><?php echo @$sty->academy; ?></td>
+													<td><?php echo @$sty->school; ?></td>
+												</tr>
+												<?php } ?>
+											</tbody>
+										</table>
+									</div>
 								<?php } ?>
 							</div>
 						</div>
 					</div>
-					<div class="panel-footer"><?php echo $this->lang->line('see_all'); ?></div>
+					<div class="panel-footer"><a href="<?php echo base_url() .'rating_list'; ?>"><?php echo $this->lang->line('see_all'); ?></a></div>
 				</div>
 			</div>
 		</div>
 
-		<div class="col-sm-6">
+		<div class="col-sm-6 col-md-6 col-lg-6">
 			<div class="panel with-nav-tabs panel-warning">
 				<div class="panel-heading">
 					<ul class="nav nav-tabs">
@@ -140,90 +146,96 @@
 						<div class="tab-content">
 							<div class="tab-pane fade in active" id="tab-my-academy">
 								<?php if($top_ten_academy != false) { ?>
-									<table class="table table-th-block margin-bottom-killer">
-										<thead>
-											<tr>
-												<th width="5%"><?php echo $this->lang->line('no'); ?></th>
-												<th><?php echo $this->lang->line('name'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('score'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('school'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('clan'); ?></th>
-											</tr>
-										</thead>
-										<tbody>
-											<?php 
-												$count = 0;
-												foreach ($top_ten_academy as $academy) {
-											?>
-											<tr>
-												<td><?php echo ++$count; ?></td>
-												<td><?php echo ucwords($academy->name); ?></td>
-												<td><span  data-toggle="tooltip" data-original-title="<?php echo $this->lang->line('xpr'),':',$academy->xpr,',', $this->lang->line('war'),':',$academy->war,',', $this->lang->line('sty'),':',$academy->sty; ?>"><?php echo $academy->total_score; ?></span></td>
-												<td><?php echo @$academy->school; ?></td>
-												<td><?php echo @$academy->clan; ?></td>
-											</tr>
-											<?php } ?>
-										</tbody>
-									</table>
+									<div class="table-responsive">
+										<table class="table table-th-block margin-bottom-killer">
+											<thead>
+												<tr>
+													<th width="5%"><?php echo $this->lang->line('no'); ?></th>
+													<th><?php echo $this->lang->line('name'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('score'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('school'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('clan'); ?></th>
+												</tr>
+											</thead>
+											<tbody>
+												<?php 
+													$count = 0;
+													foreach ($top_ten_academy as $academy) {
+												?>
+												<tr>
+													<td><?php echo ++$count; ?></td>
+													<td><?php echo ucwords($academy->name); ?></td>
+													<td><span  data-toggle="tooltip" data-original-title="<?php echo $this->lang->line('xpr'),':',$academy->xpr,',', $this->lang->line('war'),':',$academy->war,',', $this->lang->line('sty'),':',$academy->sty; ?>"><?php echo $academy->total_score; ?></span></td>
+													<td><?php echo @$academy->school; ?></td>
+													<td><?php echo @$academy->clan; ?></td>
+												</tr>
+												<?php } ?>
+											</tbody>
+										</table>
+									</div>
 								<?php } ?>
 							</div>
 
 							<div class="tab-pane fade" id="tab-my-school">
 								<?php if($top_ten_school != false) { ?>
-									<table class="table table-th-block margin-bottom-killer">
-										<thead>
-											<tr>
-												<th width="5%"><?php echo $this->lang->line('no'); ?></th>
-												<th><?php echo $this->lang->line('name'); ?></th>
-												<th width="20%"><?php echo $this->lang->line('score'); ?></th>
-												<th width="20%"><?php echo $this->lang->line('clan'); ?></th>
-											</tr>
-										</thead>
-										<tbody>
-											<?php 
-												$count = 0;
-												foreach ($top_ten_school as $school) {
-											?>
-											<tr>
-												<td><?php echo ++$count; ?></td>
-												<td><?php echo ucwords($school->name); ?></td>
-												<td><span  data-toggle="tooltip" data-original-title="<?php echo $this->lang->line('xpr'),':',$school->xpr,',', $this->lang->line('war'),':',$school->war,',', $this->lang->line('sty'),':',$school->sty; ?>"><?php echo $school->total_score; ?></span></td>
-												<td><?php echo @$school->clan; ?></td>
-											</tr>
-											<?php } ?>
-										</tbody>
-									</table>
+									<div class="table-responsive">
+										<table class="table table-th-block margin-bottom-killer">
+											<thead>
+												<tr>
+													<th width="5%"><?php echo $this->lang->line('no'); ?></th>
+													<th><?php echo $this->lang->line('name'); ?></th>
+													<th width="20%"><?php echo $this->lang->line('score'); ?></th>
+													<th width="20%"><?php echo $this->lang->line('clan'); ?></th>
+												</tr>
+											</thead>
+											<tbody>
+												<?php 
+													$count = 0;
+													foreach ($top_ten_school as $school) {
+												?>
+												<tr>
+													<td><?php echo ++$count; ?></td>
+													<td><?php echo ucwords($school->name); ?></td>
+													<td><span  data-toggle="tooltip" data-original-title="<?php echo $this->lang->line('xpr'),':',$school->xpr,',', $this->lang->line('war'),':',$school->war,',', $this->lang->line('sty'),':',$school->sty; ?>"><?php echo $school->total_score; ?></span></td>
+													<td><?php echo @$school->clan; ?></td>
+												</tr>
+												<?php } ?>
+											</tbody>
+										</table>
+									</div>
 								<?php } ?>
 							</div>
 
 							<div class="tab-pane fade" id="tab-my-clan">
 								<?php if($top_ten_clan != false) { ?>
-									<table class="table table-th-block margin-bottom-killer">
-										<thead>
-											<tr>
-												<th width="5%"><?php echo $this->lang->line('no'); ?></th>
-												<th><?php echo $this->lang->line('name'); ?></th>
-												<th width="15%"><?php echo $this->lang->line('score'); ?></th>
-											</tr>
-										</thead>
-										<tbody>
-											<?php 
-												$count = 0;
-												foreach ($top_ten_clan as $clan) {
-											?>
-											<tr>
-												<td><?php echo ++$count; ?></td>
-												<td><?php echo ucwords($clan->name); ?></td>
-												<td><span  data-toggle="tooltip" data-original-title="<?php echo $this->lang->line('xpr'),':',$clan->xpr,',', $this->lang->line('war'),':',$clan->war,',', $this->lang->line('sty'),':',$clan->sty; ?>"><?php echo $clan->total_score; ?></span></td>
-											</tr>
-											<?php } ?>
-										</tbody>
-									</table>
+									<div class="table-responsive">
+										<table class="table table-th-block margin-bottom-killer">
+											<thead>
+												<tr>
+													<th width="5%"><?php echo $this->lang->line('no'); ?></th>
+													<th><?php echo $this->lang->line('name'); ?></th>
+													<th width="15%"><?php echo $this->lang->line('score'); ?></th>
+												</tr>
+											</thead>
+											<tbody>
+												<?php 
+													$count = 0;
+													foreach ($top_ten_clan as $clan) {
+												?>
+												<tr>
+													<td><?php echo ++$count; ?></td>
+													<td><?php echo ucwords($clan->name); ?></td>
+													<td><span  data-toggle="tooltip" data-original-title="<?php echo $this->lang->line('xpr'),':',$clan->xpr,',', $this->lang->line('war'),':',$clan->war,',', $this->lang->line('sty'),':',$clan->sty; ?>"><?php echo $clan->total_score; ?></span></td>
+												</tr>
+												<?php } ?>
+											</tbody>
+										</table>
+									</div>
 								<?php } ?>
 							</div>
 						</div>
 					</div>
-					<div class="panel-footer"><?php echo $this->lang->line('see_all'); ?></div>
+					<div class="panel-footer"><a href="<?php echo base_url() .'rating_list'; ?>"><?php echo $this->lang->line('see_all'); ?></a></div>
 				</div>
 			</div>
 		</div>
